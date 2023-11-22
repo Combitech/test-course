fd034314130c87b2225a8200e02029c9f4ecb585
Wrestling Snowman <kuh49irx@duck.com>
2023-11-22 15:04:29 +0100

    x

---
+++ application/app_rl.py
@@ -0,0 +1,72 @@
+import math
+import sys
+
+PRECISION = 0.001
+MAX_LENGTH_SIDES = 1e100
+MIN_LENGHT_SIDES = 1e-100
+
+def Report_As_Equilateral():
+    print("Equilateral ", end='')
+        
+def Report_As_Isosceles():
+    print("Isosceles ", end='')
+        
+def Report_As_Right():
+    print("Right ", end='')
+            
+def Report_As_None():
+    print("None ", end='')
+        
+def Report_As_Error():
+    print("Error ", end='')
+
+def is_triangle(A, B, C):
+    # Check that sides are within range
+    if not (MIN_LENGHT_SIDES <= A <= MAX_LENGTH_SIDES
+        and MIN_LENGHT_SIDES <= B <= MAX_LENGTH_SIDES
+        and MIN_LENGHT_SIDES <= C <= MAX_LENGTH_SIDES):
+        return False
+    
+    # Check that sides are positive and not close to zero
+    if not (A > 0 and B > 0 and C > 0 
+       and not math.isclose(A, 0.0, rel_tol=PRECISION) 
+       and not math.isclose(B, 0.0, rel_tol=PRECISION)
+       and not math.isclose(C, 0.0, rel_tol=PRECISION)):
+       return False
+
+    # Check that this is a triangle
+    if (A + B) > C and (B + C) > A and (C + A) > B:
+        return True
+    else:
+        return False
+
+
+# Triangle function
+def Tri(A, B, C):
+    if not is_triangle(A,B,C):
+        Report_As_Error()
+        return
+
+    '''
+       Write your code here!
+    '''
+        
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 4):
+        Report_As_Error()
+        sys.exit(0)
+
+    try:
+        As = float(sys.argv[1])
+        Bs = float(sys.argv[2])
+        Cs = float(sys.argv[3])
+    except:
+        Report_As_Error()
+        sys.exit(0)
+
+    Tri(As, Bs, Cs)
+
+    sys.exit(0)
+
